3b59f0caa2fa8bf5e1b56617c5d7b89ee37ccca2
BlisProgrammer <blisprogrammer@gmail.com>
2025-08-26 13:58:37 +0800

    Quick fix: about does not work on landscape

--- app/src/main/java/com/blis/customercity/AboutFragment.java
+++ app/src/main/java/com/blis/customercity/AboutFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
@@ -17,7 +18,7 @@ public class AboutFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        RelativeLayout linearLayout = (RelativeLayout) inflater.inflate(R.layout.about, container, false);
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.about, container, false);
         Button backButton = linearLayout.findViewById(R.id.back_button);
         backButton.setOnClickListener(v -> {
             Main main = (Main) getActivity();
